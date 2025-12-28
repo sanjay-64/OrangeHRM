fd182d95c241eeac3efad9fd6824bee194252241
sanjay-64 <sanjaykumar536601@gmail.com>
2025-12-28 21:53:52 +0530

    Initial commit: Selenium TestNG Maven framework setup

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,45 @@
+package tests;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test(invocationCount = 1, threadPoolSize = 20)
+    public void Loginhrm() {
+    	System.out.println("Thread ID: " + Thread.currentThread().getId());
+
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Username
+        WebElement userName = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+        userName.clear();
+        userName.sendKeys("Admin");
+
+        // Password
+        WebElement userPwd = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.name("password")));
+        userPwd.clear();
+        userPwd.sendKeys("admin123");
+
+        // Login button
+        WebElement loginButton = wait.until(
+                ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']")));
+        loginButton.click();
+
+        driver.quit();
+    }
+}
